a1f1786ece72dc563b300c9e2cc1e70521143477
EdenSigma360 <83540363+EdenSigma360@users.noreply.github.com>
2022-06-15 15:19:05 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompAComponent } from './comp-a/comp-a.component';
+import { CompBComponent } from './comp-b/comp-b.component';
+import { SelectLanguageComponent } from './select-language/select-language.component';
+import { I18nModule } from './i18n/i18n.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompAComponent,
+    CompBComponent,
+    SelectLanguageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, I18nModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/i18n/i18n.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  TranslateCacheModule,
+  TranslateCacheSettings,
+  TranslateCacheService,
+} from 'ngx-translate-cache';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    TranslateCacheModule.forRoot({
+      cacheService: {
+        provide: TranslateCacheService,
+        useFactory: translateCacheFactory,
+        deps: [TranslateService, TranslateCacheSettings],
+      },
+      cacheMechanism: 'Cookie',
+    }),
+  ],
+  exports: [TranslateModule],
+})
+export class I18nModule {
+  constructor(
+    translate: TranslateService,
+    translateCacheService: TranslateCacheService
+  ) {
+    translateCacheService.init();
+    translate.addLangs(['en', 'ru', 'he']);
+    const browserLang =
+      translateCacheService.getCachedLanguage() || translate.getBrowserLang();
+    // const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|ru|he/) ? browserLang : 'en');
+  }
+}
+
+export function translateLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+export function translateCacheFactory(
+  translateService: TranslateService,
+  translateCacheSettings: TranslateCacheSettings
+) {
+  return new TranslateCacheService(translateService, translateCacheSettings);
+}
