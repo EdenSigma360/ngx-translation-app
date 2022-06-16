cae044e4b166f22958218c2f8fda815e9f475595
EdenSigma360 <83540363+EdenSigma360@users.noreply.github.com>
2022-06-16 14:23:17 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  dir: string = 'ltr';
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      if (event.lang == 'he') this.dir = 'rtl';
+      else this.dir = 'ltr';
+    });
+  }
   title = 'ngx-translate-app';
 }
